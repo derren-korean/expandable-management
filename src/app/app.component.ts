3b0242c90b694ce36befa4153d07ea2d58d7ef8a
Unknown <derren832@gmail.com>
2019-10-30 20:59:37 +0900

    add auth
    add auth and user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { Plugins, Capacitor } from '@capacitor/core';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AuthService } from './auth/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -13,15 +16,25 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private authService: AuthService,
+    private router: Router
   ) {
     this.initializeApp();
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
+      if (Capacitor.isPluginAvailable('SplashScreen')) {
+        Plugins.SplashScreen.hide();
+      }
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
+  }
+
+  onLogout() {
+    this.authService.logout();
+    this.router.navigateByUrl('/auth');
   }
 }
